043d21e5a8774d8c4eb6d0c4b6ee3d476e51a27a
kaduvizu <kaduvizu@hotmail.com>
2026-04-12 18:32:44 -0300

    feat: signals em usercomponent, porem comentado, não vamos usar agora

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, computed, Input, input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -11,12 +11,18 @@ export class UserComponent {
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
 
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
   get imagePath() {
     return `/assets/users/${this.avatar}`;
   }
 
-  onSelectUser() {
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar();
+  // })
 
+  onSelectUser() {
   }
 
 }
